5770ba3d95cffebddf304e10192c81e925610925
Mhasnainirfan <hasnainirfan07@gmail.com>
2023-12-05 17:30:47 +0500

    commits

---
+++ fake/src/App.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react';
+import './App.css'
+
+
+const App=()=>{
+  const [fake, setFake]=useState([]);
+  console.log(fake);
+  useEffect(()=>{
+     fakestore();
+  },[])
+
+  const fakestore=async()=>{
+    const response=await fetch("https://fakestoreapi.com/products")
+    // console.log(response)
+    const jsonData=await response.json();
+    // console.log(jsonData);
+    setFake(jsonData); }
+   fakestore();
+  return(
+    <>
+    <h2>Fake api </h2>
+    <div className='container'>
+      {fake.map((values)=>{
+return(
+<>
+<div className='box'>
+        <div className='content'>
+          <h5>{values.title}</h5>
+          <p>{values.description}</p>
+        </div>
+        <img src={values.image}alt=''/>
+
+</div>
+</>
+)
+      })}
+      </div>
+    </>
+  )
+}
+
+
+export default App;
